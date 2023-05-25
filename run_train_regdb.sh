11679b2cada7a4308f4370fcbc0c9af996f517c7
zesenwu23 <117629734+zesenwu23@users.noreply.github.com>
2023-05-25 21:51:48 +0800

    Update run_train_regdb.sh

--- run_train_regdb.sh
+++ run_train_regdb.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python train_regdb.py -b 256 -a agw -d  regdb_rgb \
 --iters 100 --num-instances 16 \
 --data-dir "/data0/ReIDData/RegDB" \
---logs-dir "/data1/wzs/adca-regdb/regdb/origin-agw-momentum02-benchmarkFalse-cj05-step10epoch30" \
+--logs-dir "/data1/cvpr23_upload/origin/regdb" \
 --trial 1
 
 # trial: 1,2,3,4,5,6,7,8,9,10
